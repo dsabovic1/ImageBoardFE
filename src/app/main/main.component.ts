6a4705a0b292797523da4791bfa3b8391fec3d9c
dsabovic1 <dsabovic1@etf.unsa.ba>
2020-08-12 21:18:49 +0200

    Header toggle

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -7,11 +8,32 @@ import { AuthService } from 'src/app/auth.service';
   styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  isLoggedIn: boolean;
 
-  ngOnInit(): void {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {
+    this.router.events.subscribe((ev) => {
+      if (ev instanceof NavigationEnd) {
+        this.isLoggedIn = this.checkIfLoggedIn();
+        console.log(this.isLoggedIn);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.checkIfLoggedIn();
+  }
 
   onLogout() {
     this.authService.logout();
+    this.isLoggedIn = this.checkIfLoggedIn();
+    this.cdr.detectChanges();
+  }
+
+  checkIfLoggedIn() {
+    return this.authService.isLoggedIn();
   }
 }
